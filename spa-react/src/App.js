fe36de202f66398759186d3342644470804b7be0
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-14 15:51:22 +0630

    Ep-9-Detail-concept-of-stylesheet-in-react

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -4,16 +4,9 @@ import image from "./assets/image.png";
 function App() {
   let name = "Nay Zaw Aung";
   return (
-    <div>
+    <div className="app">
       <h1>Hello {name}</h1>
-      {/* public way */}
-      {/* absolute path */}
-      <img src="http://localhost:3000/image.png" alt="" />
-      {/* relative path */}
-      <img src="/image.png" alt="" />
-
-      {/* import way */}
-      <img src={image} alt="" />
+      
     </div>
   );
 }
