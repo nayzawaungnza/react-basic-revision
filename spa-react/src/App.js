0291d7a196abb3a74b99314fa3c1229f5c6e53d1
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-14 15:42:44 +0630

    Ep-8-two-way-of-using-images-in-react

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
+import image from "./assets/image.png";
 
 function App() {
   let name = "Nay Zaw Aung";
-  return <h1>Hello {name}</h1>;
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      {/* public way */}
+      {/* absolute path */}
+      <img src="http://localhost:3000/image.png" alt="" />
+      {/* relative path */}
+      <img src="/image.png" alt="" />
+
+      {/* import way */}
+      <img src={image} alt="" />
+    </div>
+  );
 }
 
 export default App;
