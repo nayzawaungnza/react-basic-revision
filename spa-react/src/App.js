d61316e5cb8c59b02903d3b5ed9aab055908784e
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-15 15:22:00 +0630

    Ep-19-React-Default-Children-Prop

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -3,6 +3,7 @@ import image from "./assets/image.png";
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import PostList from "./components/PostList";
+import Modal from "./components/Modal";
 function App() {
   let [posts, setPosts] = useState([
     {
@@ -23,6 +24,16 @@ function App() {
     <>
       <Navbar />
       <PostList posts={posts} />
+      {/* <Modal>
+        <h1>Zoom class is available now.</h1>
+        <p>
+          feel free to <a>join</a> here.
+        </p>
+      </Modal> */}
+      <Modal>
+        <h1>Term and Condition</h1>
+        <p>loerm</p>
+      </Modal>
     </>
   );
 }
