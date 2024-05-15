dfdef0f316075e2857e8d0d8c10bd5432058ddf9
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-15 13:35:42 +0630

    Ep-10-why-do-we-need-state

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -3,10 +3,14 @@ import image from "./assets/image.png";
 
 function App() {
   let name = "Nay Zaw Aung";
+  let changeName = () => {
+    name = "Aung Aung";
+    console.log(name);
+  };
   return (
     <div className="app">
       <h1>Hello {name}</h1>
-      
+      <button onClick={changeName}>change name</button>
     </div>
   );
 }
