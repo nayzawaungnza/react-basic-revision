6028364942439ff13443dae5f0c9b2ee5432785a
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-16 11:12:57 +0630

    Ep-26-React-2-way-data-binding

--- spa-react/src/components/PostForm/index.js
+++ spa-react/src/components/PostForm/index.js
@@ -6,16 +6,27 @@ export default function PostForm() {
   //     setTitle(e.target.value);
   //     console.log(e.target.value);
   //   };
+  let resetForm = () => {
+    setTitle("");
+    console.log("updated success");
+  };
   return (
     <form className="post-form">
       <h1>Create Post</h1>
       <div className="form-control">
         <label>Title</label>
         {/* <input type="text" onChange={onChangeHandler} /> //same under method */}
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
         <p>{title}</p>
       </div>
       <div className="form-control">
+        <button type="button" onClick={resetForm}>
+          reset
+        </button>
         <button>Create Post</button>
       </div>
     </form>
