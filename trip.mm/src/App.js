231a2ee0041bee050a0f8618b4b17539d8a7b2a2
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-17 10:59:55 +0630

    Ep-37-how-react-rendering-work-detail

--- trip.mm/src/App.js
+++ trip.mm/src/App.js
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TripList from "./components/TripList";
 
 function App() {
-  let [show, setShow] = useState(false);
+  let [data, setData] = useState("my data");
+  let myFunction = () => {
+    setData("update data");
+  };
+  useEffect(() => {
+    myFunction();
+    console.log("running");
+  }, [myFunction]);
   return (
     <div className="App">
       <h1>Hello Trip.mm</h1>
-      <button onClick={() => setShow(true)}>hide trip</button>
-      {!show && <TripList />}
+      <TripList />
     </div>
   );
 }
 
 export default App;
+//react is not rerender in same data
