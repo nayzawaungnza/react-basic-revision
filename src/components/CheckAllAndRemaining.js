760a3d1f69aaac44490f596e4777028acb931ae0
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-20 09:46:42 +0630

    Ep-39-Api-Setup-And-Component-Extraction

---
+++ src/components/CheckAllAndRemaining.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CheckAllAndRemaining() {
+  return (
+    <>
+      <div className="check-all-container">
+        <div>
+          <div className="button">Check All</div>
+        </div>
+
+        <span>3 items remaining</span>
+      </div>
+    </>
+  );
+}
