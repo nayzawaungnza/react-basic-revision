7534110049c6ace4734b89532eadffc2b9e70e03
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-20 09:58:12 +0630

    Ep-40-Fetch-all-Todos

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import TodoList from "./components/TodoList";
 import CheckAllAndRemaining from "./components/CheckAllAndRemaining";
 import TodoFilters from "./components/TodoFilters";
 import ClearCompletedBtn from "./components/ClearCompletedBtn";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
         <TodoForm />
 
-        <TodoList />
+        <TodoList todos={todos} />
 
         <CheckAllAndRemaining />
 
