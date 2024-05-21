cef4d6a9ae67523590a79b0a2ed2e78510b4619b
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-21 10:11:54 +0630

    Ep-50-fetch-all-blogs

--- blogs/src/pages/Home.js
+++ blogs/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import "./Home.css";
 
 export default function Home() {
+  let { data: blogs, loading, error } = useFetch("http://localhost:3001/blogs");
   return (
-    <div>
+    <div className="Home">
       <h1>Home</h1>
+      {error && <p>{error}</p>}
+      {loading && <p>loading ....</p>}
+      {blogs &&
+        blogs.map((blog) => (
+          <div key={blog.id} className="card">
+            <h3>{blog.title}</h3>
+            <p> posted by - {blog.author}</p>
+            <a href="">Read More</a>
+          </div>
+        ))}
     </div>
   );
 }
