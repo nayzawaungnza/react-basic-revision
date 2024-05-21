617a7b4de121afd7c0a7d1bdadf204af1f352ab6
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-21 09:52:40 +0630

    Ep-49-React-Router-setup-and-using-Links

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,14 +1,24 @@
+import { NavLink, Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <nav>
+        <h1>My Blogs</h1>
         <ul>
-          <li>Home</li>
-          <li>About</li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
         </ul>
       </nav>
+      <Outlet />
     </div>
   );
 }
