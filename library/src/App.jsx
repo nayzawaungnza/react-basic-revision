adda7f196a6afd090e4bcc4a34c49959236a2b91
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-21 14:38:47 +0630

    Ep-53-install-react-with-vite-and-tailwind

---
+++ library/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-500'>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
