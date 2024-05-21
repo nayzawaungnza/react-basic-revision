0dc0f3cb03f2731a7b4fcccc1db2c3ec0631a00a
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-21 15:23:39 +0630

    Ep-56-Home-Page-Designs

--- library/src/pages/Home.jsx
+++ library/src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import { useState } from 'react'
-
+import HeroSection from './components/HeroSection'
+import book from '../assets/surja-sen-das-raj.jpg'
 function Home() {
 
   return (
     <>
-      <h1 className='text-blue-500'>Hello World!</h1>
+    <HeroSection/>
+    <div className='grid grid-cols-2 md:grid-cols-4 gap-4 my-3'>
+        {[1, 2, 3, 4, 5].map(() => (
+          <div className='p-4 border border-1'>
+
+            <img src={book} alt="" />
+            <div className='text-center space-y-2 mt-3'>
+              <h1>Book title</h1>
+              <p>description</p>
+              {/* genres */}
+              <div className='flex flex-wrap'>
+                {['travel', 'knowledge', 'travel', 'knowledge'].map(genre => (
+                  <span className='mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500'> {genre}</span>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
