8dc0973c886502de0db420ccb309694043a4667d
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-21 09:08:51 +0630

    Ep-48-SPA-intro-and-project-setup

---
+++ blogs/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
