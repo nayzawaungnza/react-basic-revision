77153ffff4348133f5739e09dbc65c521850ffbc
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-24 10:14:24 +0630

    Ep-61-Book-Create-Form-Design

--- library/src/pages/Create.jsx
+++ library/src/pages/Create.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Create() {
+  let [title, setTitle] = useState();
+  let [description, setDescription] = useState();
+  let [newCategory, setNewCategory] = useState([]);
   return (
-    <div>
-      Create
-    </div>
-  )
+    <>
+      <form className="w-full max-w-lg mx-auto">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-title"
+            >
+              Book Title {title}
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-title"
+              type="text"
+              placeholder="enter title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-des"
+            >
+              Book Description {description}
+            </label>
+            <textarea
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-des"
+              placeholder="enter description"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Book Categories {newCategory}
+            </label>
+            <div className="flex items-center space-x-2">
+              <input
+                value={newCategory}
+                onChange={(e) => setNewCategory(e.target.value)}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                type="text"
+                placeholder="Enter Book Category"
+              />
+              <button className="bg-primary p-2 rounded mb-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <button className="flex items-center gap-1 w-full text-white bg-primary px-3 py-2 rounded justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+
+          <span className="hidden md:block">Create Book</span>
+        </button>
+      </form>
+    </>
+  );
 }
