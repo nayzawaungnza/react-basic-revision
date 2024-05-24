867581c373124c4e6ac9d35be7e86502be936468
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-24 14:58:11 +0630

    Ep-64-add-book-search-feature

--- library/src/pages/components/BookList.jsx
+++ library/src/pages/components/BookList.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import bookimage from '../../assets/surja-sen-das-raj.jpg'
 import useFetch from '../../hooks/useFetch'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function BookList() {
-    let {data: books, loading, error}=useFetch('http://localhost:3001/books');
+  let location = useLocation();
+  let params = new URLSearchParams(location.search);
+  let search = params.get('search');
+    let {data: books, loading, error}=useFetch(`http://localhost:3001/books${search ? `?q=${search}` :''}`);
     if (error) {
         return <p>{error}</p>;
     }
