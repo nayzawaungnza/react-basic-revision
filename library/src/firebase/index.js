96b11d7bb76399f4f381d9ffc33fb2eac708c400
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-27 10:28:42 +0630

    Ep-73-setup-firebase-project-and-connect

---
+++ library/src/firebase/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbQnY2Qr45DLmGjzl1Wtb4ojQgn9qUGuo",
+  authDomain: "library-app-739ac.firebaseapp.com",
+  projectId: "library-app-739ac",
+  storageBucket: "library-app-739ac.appspot.com",
+  messagingSenderId: "729893388849",
+  appId: "1:729893388849:web:bfcd1982fe7bf192674b10",
+  measurementId: "G-TB488XLBHD",
+};
+const app = initializeApp(firebaseConfig);
