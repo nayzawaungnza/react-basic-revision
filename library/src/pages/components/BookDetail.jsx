f44edfa209af54e7988da8174b3fc796b91bb84f
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-29 11:14:11 +0630

    Ep-94-Add-note-feature

--- library/src/pages/components/BookDetail.jsx
+++ library/src/pages/components/BookDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import useTheme from "../../hooks/useTheme";
 import useFirestore from "../../hooks/useFirestore";
+import NoteForm from "./NoteForm";
 export default function BookDetail() {
   let { id } = useParams();
   let { getDocument } = useFirestore();
@@ -55,20 +56,7 @@ export default function BookDetail() {
             </div>
           </div>
           <div className="space-y-3">
-            <div className="w-96 mx-auto">
-              <div className="relative w-full min-w-[200px]">
-                <textarea
-                  className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-500 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-1 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-300 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
-                  placeholder=" "
-                ></textarea>
-                <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                  My Note
-                </label>
-              </div>
-              <button className="text-white bg-primary px-3 py-2 rounded flex items-center gap-1">
-                <span className="hidden md:block">add note</span>
-              </button>
-            </div>
+            <NoteForm />
             <div className="max-w-md mx-auto border px-6 py-4 rounded-lg">
               <div className="flex items-center mb-6">
                 <img
@@ -94,21 +82,21 @@ export default function BookDetail() {
                     href="#"
                     className="text-gray-500 hover:text-gray-700 mr-4"
                   >
-                    <i class="far fa-thumbs-up"></i> Like
+                    <i className="far fa-thumbs-up"></i> Like
                   </a>
                   <a href="#" className="text-gray-500 hover:text-gray-700">
-                    <i class="far fa-comment-alt"></i> Reply
+                    <i className="far fa-comment-alt"></i> Reply
                   </a>
                 </div>
-                <div class="flex items-center">
+                <div className="flex items-center">
                   <a
                     href="#"
                     className="text-gray-500 hover:text-gray-700 mr-4"
                   >
-                    <i class="far fa-flag"></i> Report
+                    <i className="far fa-flag"></i> Report
                   </a>
                   <a href="#" className="text-gray-500 hover:text-gray-700">
-                    <i class="far fa-share-square"></i> Share
+                    <i className="far fa-share-square"></i> Share
                   </a>
                 </div>
               </div>
---
+++ library/src/pages/components/NoteForm.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import useFirestore from "../../hooks/useFirestore";
+import { useParams } from "react-router-dom";
+
+export default function NoteForm() {
+  let { id } = useParams();
+  let { user } = useContext(AuthContext);
+  let [body, setBody] = useState("");
+  let { addCollection } = useFirestore();
+
+  let addNote = async (e) => {
+    e.preventDefault();
+    let note = {
+      body: body,
+      bookUid: id,
+      userUid: user.uid,
+    };
+    await addCollection("notes", note);
+    setBody("");
+  };
+  return (
+    <form onSubmit={addNote}>
+      <div className="w-96 mx-auto">
+        <div className="relative w-full min-w-[200px]">
+          <textarea
+            className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-500 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-1 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-300 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+            placeholder=" "
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+          <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+            My Note
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-primary px-3 py-2 rounded flex items-center gap-1"
+        >
+          <span className=" md:block">add note</span>
+        </button>
+      </div>
+    </form>
+  );
+}
